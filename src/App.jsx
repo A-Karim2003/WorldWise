8612edc4fc929d7de77f3b85a37ef0ca406b6f08
A-Karim2003 <17a.karim@gmail.com>
2025-09-30 14:00:07 +0100

    feat: Create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      {/* Declare links to pages*/}
+      <Link to="home">Home</Link>
+      <Link to="Pricing">Pricing</Link>
+      <Link to="home">Home</Link>
+
+      {/* Declare the routes */}
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/pricing" element={<Pricing />}></Route>
+        <Route path="/product" element={<Product />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Product.jsx
@@ -0,0 +1,9 @@
+function Product() {
+  return (
+    <div>
+      <div>Product</div>
+    </div>
+  );
+}
+
+export default Product;
