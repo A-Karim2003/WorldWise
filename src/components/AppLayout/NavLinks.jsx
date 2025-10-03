a845f4c81cac4d13f6a095854cf3f2090b534cbc
Abdoulkarim <17a.karim@gmail.com>
2025-10-03 08:21:52 +0100

    feat: complete homepage section, replace btn element for a resuable btn component

--- src/components/AppLayout/NavLinks.jsx
+++ src/components/AppLayout/NavLinks.jsx
@@ -1,5 +1,6 @@
 import style from "./AppLayout.module.css";
 import { NavLink } from "react-router-dom";
+import Button from "./Button";
 
 function NavLinks() {
   return (
@@ -10,8 +11,8 @@ function NavLinks() {
       <NavLink className={style.navLink} to="/product">
         Product
       </NavLink>
-      <NavLink className={`${style.navLink} ${style.navBtn}`} to="/login">
-        Login
+      <NavLink className={`${style.navLink} `} to="/login">
+        <Button>Login</Button>
       </NavLink>
     </nav>
   );
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,22 @@
-import s from "./homepage.module.css";
+import styles from "./homepage.module.css";
+import Button from "../../components/AppLayout/Button";
 
 function Homepage() {
+  console.log(styles);
+
   return (
-    <div className={s.background}>
-      <h1>Home</h1>
+    <div className={styles.homepage}>
+      <div className={styles.content}>
+        <h1>
+          You travel the world. <br /> WorldWise keeps track of your adventures.
+        </h1>
+        <p>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </p>
+        <Button> START TRACKING NOW </Button>
+      </div>
     </div>
   );
 }
