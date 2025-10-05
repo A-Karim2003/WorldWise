6f7480edbee5e5327c7fc5f0de0944b19be65d97
Abdoulkarim <17a.karim@gmail.com>
2025-10-05 18:10:39 +0100

    feat: create login form component

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,23 @@
+import styles from "./login.module.css";
+import Button from "../../components/AppLayout/Button";
+
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <section className={styles.loginContainer}>
+      <form className={styles.loginForm}>
+        <div className={styles.formGroup}>
+          <label htmlFor="email">Email:</label>
+          <input id="email" type="text" placeholder="Email" />
+        </div>
+
+        <div className={styles.formGroup}>
+          <label htmlFor="password">Password:</label>
+          <input id="password" type="password" placeholder="Password" />
+        </div>
+
+        <Button>Login</Button>
+      </form>
+    </section>
   );
 }
 
